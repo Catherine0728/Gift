835fe5ccaad69d500b2c2e594b0d4a2f5f767496
Boxfish <15520054289@163.com>
2015-12-12 21:03:46 +0800

    init

---
+++ Gift/app/src/main/java/com/example/catherine/gift/GiftApplication.java
@@ -0,0 +1,10 @@
+package com.example.catherine.gift;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by catherine on 15/12/12.
+ */
+public class GiftApplication extends MultiDexApplication{
+
+}
---
+++ Gift/app/src/main/java/com/example/catherine/gift/ui/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.catherine.gift.ui.activity;
+
+
+import com.example.catherine.gift.R;
+import com.example.catherine.gift.ui.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public void getArgs() {
+
+    }
+
+    @Override
+    public void setView() {
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public void setComponent() {
+
+    }
+}
